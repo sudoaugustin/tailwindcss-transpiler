76672f9c4426207974809efb7fd0108f7171f30f
Auguestin Joseph <auguestin304@gmail.com>
2020-09-23 15:20:11 +0630

    Shipping the v.0.0.1

---
+++ vscode/src/extension.js
@@ -0,0 +1,20 @@
+const vscode = require('vscode');
+const { handleInit, handleSave } = require('./handlers/onSaveHandler');
+
+function activate(context) {
+  handleInit();
+  let disposableCompileOnSave = vscode.workspace.onDidSaveTextDocument(
+    ({ languageId, fileName }) =>
+      handleSave({
+        filePath: fileName,
+        rootPath: vscode.workspace.rootPath,
+        isSASS: languageId === 'scss' || languageId === 'sass',
+      })
+  );
+  context.subscriptions.push(disposableCompileOnSave);
+}
+exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = { activate, deactivate };
